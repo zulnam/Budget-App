b2868086d47b388d7ed877066d771bc1ee7ca1b9
VNecsescu <valentin.necsescu@dunelm.com>
2019-01-09 20:21:54 +0000

    removed console logs; dummy data

--- src/app.js
+++ src/app.js
@@ -1,40 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import 'react-dates/lib/css/_datepicker.css';
-import './styles/styles.scss';
-//component
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import {addExpense} from "./actions/expenses";
-import {setTextFilter, sortByAmount} from "./actions/filters";
+import "react-dates/lib/css/_datepicker.css";
+import "./styles/styles.scss";
+//components
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter, sortByAmount } from "./actions/filters";
 import getVisibleExpenses from "./selectors/getVisibleExpenses";
-
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 1000,  createdAt: 2500}));
-store.dispatch(addExpense({description: 'Rent', amount: 450,  createdAt: 25000}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 500, createdAt: 11000}));
-
-
-// store.dispatch(setTextFilter('water'));
-// store.dispatch(sortByAmount());
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-// console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
-
-console.log(store.getState().expenses);
-
 const jsx = (
-    <Provider store={store}>
-        <AppRouter/>
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
-
-
+ReactDOM.render(jsx, document.getElementById("app"));
